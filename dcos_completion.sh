592d212973798d5323807bdb9db02224abf13d00
Stuart Leeks <stuart@leeks.net>
2016-10-23 19:19:00 +0100

    Finish initial marathon command implementation

--- dcos_completion.sh
+++ dcos_completion.sh
@@ -35,7 +35,7 @@ __dcos_to_extglob() {
 }
 
 __dcos_log(){
-    echo "$1" >> ./temp.txt
+    # echo "$1" >> ./temp.txt
     return 0;
 }
 
@@ -52,13 +52,6 @@ __dcos_log child.Command.chain=$currentCommandChain
 
 	while [ $counter -le $cword ]; do
 		case "${words[$counter]}" in
-# 			"")
-# 				local completions_func=_dcos${currentCommandChain}
-# __dcos_log childCommand.invoke=$completions_func            
-# 				declare -F $completions_func >/dev/null && $completions_func
-# __dcos_log childCommand.retis.$ret 
-# 				return $ret
-# 				;;
 			"")
 __dcos_log childCommand.ret1
 				return 1
@@ -81,37 +74,147 @@ __dcos_log childCommand.ret1
 	return 1
 }
 
+__dcos_complete_marathon_app_ids(){
+	local apps=( $(dcos marathon app list --json | jq --raw-output ".[] | .id") )
+	COMPREPLY=( $(compgen -W "${apps[*]}" -- "$cur") )
+}
+
+
 _dcos_auth(){
     local subcommands="
         login
         logout
     "
-__dcos_log auth.enter.
 
-   	__dcos_childCommand "$subcommands" && return
-
-__dcos_log auth.noChildMatch.cur=$cur.
 	case "$cur" in
 		-*)
-__dcos_log auth.matchSwitch.
 			COMPREPLY=( $( compgen -W "--help --info" -- "$cur" ) )
 			;;
 		*)
-__dcos_log auth.matchCommand.
 			COMPREPLY=( $( compgen -W "$subcommands" -- "$cur" ) )
 			;;
 	esac
-
 	return 0;
 }
 # TODO - config
 # TODO - help
 # TODO - job
 
-# TODO marathon-app-add file completion
-# _dcos_marathon_app_add(){
-# 	COMPREPLY=( $( compgen -W "--help --info --config-schema" -- "$cur" ) )
-# }
+_dcos_marathon_app_about(){
+	return 0; # suppress completion
+}
+_dcos_marathon_app_add(){
+	COMPREPLY=( $( compgen -o filenames -A file -- "$cur" ) )
+	return 0;
+}
+_dcos_marathon_app_list(){
+	COMPREPLY=( $( compgen -W "--json" -- "$cur" ) )
+	return 0;
+}
+_dcos_marathon_app_remove(){
+	case "$cur" in
+		-*)
+			COMPREPLY=( $( compgen -W "--force" -- "$cur" ) )
+			;;
+		*)
+			cur="${cur##*=}"
+			__dcos_complete_marathon_app_ids
+			return 0
+			;;
+	esac
+	return 0;
+}
+_dcos_marathon_app_restart(){
+	case "$cur" in
+		-*)
+			COMPREPLY=( $( compgen -W "--force" -- "$cur" ) )
+			;;
+		*)
+			cur="${cur##*=}"
+			__dcos_complete_marathon_app_ids
+			return 0
+			;;
+	esac
+	return 0;
+}
+_dcos_marathon_app_show(){
+	case "$cur" in
+		-*)
+			COMPREPLY=( $( compgen -W "--app-version=" -- "$cur" ) ) # TODO complete version numbers
+			;;
+		*)
+			cur="${cur##*=}"
+			__dcos_complete_marathon_app_ids
+			return 0
+			;;
+	esac
+	return 0;
+}
+_dcos_marathon_app_start(){
+	case "$cur" in
+		-*)
+			COMPREPLY=( $( compgen -W "--force" -- "$cur" ) )
+			;;
+		*)
+			cur="${cur##*=}"
+			__dcos_complete_marathon_app_ids
+			return 0
+			;;
+	esac
+	return 0;
+}
+_dcos_marathon_app_stop(){
+	case "$cur" in
+		-*)
+			COMPREPLY=( $( compgen -W "--force" -- "$cur" ) )
+			;;
+		*)
+			cur="${cur##*=}"
+			__dcos_complete_marathon_app_ids
+			return 0
+			;;
+	esac
+	return 0;
+}
+_dcos_marathon_app_kill(){
+	case "$cur" in
+		-*)
+			COMPREPLY=( $( compgen -W "--scale --host=" -- "$cur" ) ) ## TODO complete host
+			;;
+		*)
+			cur="${cur##*=}"
+			__dcos_complete_marathon_app_ids
+			return 0
+			;;
+	esac
+	return 0;
+}
+_dcos_marathon_app_list(){
+	case "$cur" in
+		-*)
+			COMPREPLY=( $( compgen -W "--max-count= " -- "$cur" ) )
+			;;
+		*)
+			cur="${cur##*=}"
+			__dcos_complete_marathon_app_ids
+			return 0
+			;;
+	esac
+	return 0;
+}
+_dcos_marathon_app_update(){
+	case "$cur" in
+		-*)
+			COMPREPLY=( $( compgen -W "--force" -- "$cur" ) )
+			;;
+		*)
+			cur="${cur##*=}"
+			__dcos_complete_a__dcos_complete_marathon_app_idspp_ids
+			return 0
+			;;
+	esac ## TODO complete properties
+	return 0;
+}
 _dcos_marathon_app(){
     local subcommands="
 		about
